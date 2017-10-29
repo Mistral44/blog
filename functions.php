6c8e90b515ccc35d874b44e6aaa8784eb09fc9ba
mistral44 <sergeyshaydo@gmail.com>
2017-10-29 14:16:12 +0200

    third

--- functions.php
+++ functions.php
@@ -601,7 +601,7 @@ function wpajax_list_comment($comment, $args, $depth) {
         $tag       = 'li';
         $add_below = 'div-comment';
     }?>
-    <?php echo $tag; comment_class( empty( $args['has_children'] ) ? '' : 'parent' ); ?> id="comment-<?php comment_ID() ?>"><?php 
+    <<?php echo $tag; comment_class( empty( $args['has_children'] ) ? '' : 'parent' ); ?> id="comment-<?php comment_ID() ?>"><?php 
     if ( 'div' != $args['style'] ) { ?>
         <div id="div-comment-<?php comment_ID() ?>" class="comment-body"><?php
     } ?>
